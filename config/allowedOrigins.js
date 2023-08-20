fbbc96793e9140de4f14d890c393ac9154b43a0a
altai861 <altaizandanbileg@gmail.com>
2023-08-20 14:51:05 +0800

    Added origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://www.bloggy.com'
+    'https://www.bloggy.com',
+    'https://lifeblog.onrender.com/',
 ]
 
 module.exports = allowedOrigins
