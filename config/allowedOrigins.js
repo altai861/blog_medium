47f2d9c038686c454e42abd30874036b56f1e0b8
altai861 <altaizandanbileg@gmail.com>
2023-08-20 14:56:41 +0800

    changed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'https://www.bloggy.com',
-    'https://lifeblog.onrender.com/',
+    'https://lifeblog.onrender.com',
 ]
 
 module.exports = allowedOrigins
